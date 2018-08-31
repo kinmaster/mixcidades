3962fe5f4a3c8e4f60a3bf5fe46887f206918628
lucian <lucian@localhost.localdomain>
2018-08-31 17:36:09 -0300

    bugs2

--- db/migrate/20180831184007_remove_colum_city_to_user.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumCityToUser < ActiveRecord::Migration[5.1]
-  def change
-    remove_foreign_key :users, :city
-  end
-end
--- db/migrate/20180831193914_remove_city_to_user.rb
+++
@@ -1,6 +0,0 @@
-class RemoveCityToUser < ActiveRecord::Migration[5.1]
-  def change
-  
-    remove_column :users, :city, :string
-    end
-end
